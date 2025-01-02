44552bba370dec1a0b3d6403a0586a3e3123ac62
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-01-02 20:53:58 +0530

    connect mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { connectToDB } from "./db/db.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
+connectToDB();
 
 app.get('/', (req, res) => {
     res.send("hello world");
