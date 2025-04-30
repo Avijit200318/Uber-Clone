099ac43691e4f418a79559b577ca8ef1f6f25a6a
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-04-30 11:36:36 +0530

    updated captain details page UI

--- Frontend/src/components/CaptainDetails.jsx
+++ Frontend/src/components/CaptainDetails.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import {useSelector} from "react-redux";
 
 export default function CaptainDetails() {
+
+  const {currentCaptain} = useSelector((state)=> state.captain);
+  console.log("currentCaptain: ", currentCaptain);
+
   return (
     <div>
       <div className="flex justify-between">
-          <div className="flex items-center justify-start gap-3">
+          <div className="flex items-center justify-start gap-3 w-3/4">
             <img src="https://assets-us-01.kc-usercontent.com/5cb25086-82d2-4c89-94f0-8450813a0fd3/0c3fcefb-bc28-4af6-985e-0c3b499ae832/Elon_Musk_Royal_Society.jpg" alt="" className="h-10 w-10 rounded-full object-cover" />
-            <h4 className='font-medium line-clamp-2 w-1/2'>Hitesh Choudhury</h4>
+            <h4 className='line-clamp-2 w-1/2 capitalize font-semibold'>{currentCaptain.fullName.firstName + ' ' + currentCaptain.fullName.lastName}</h4>
           </div>
-          <div className="w-1/2 flex justify-center items-end flex-col">
+          <div className="w-1/4 flex justify-center items-end flex-col">
             <h4 className='text-xl font-semibold'>₹ 192.50</h4>
             <p className='text-sm font-semibold text-gray-600'>Earned</p>
           </div>
